ec9da2b7c4e0d1dda66675d70b2660ede9328753
vinhnguyenhq <nhquocvinh@gmail.com>
2018-01-29 16:00:47 +0700

    add puppeteer

--- selenium.js
+++ selenium.js
@@ -8,10 +8,22 @@ var client = webdriverio.remote(options);
 
 client
   .init()
-  .url('https://duckduckgo.com/')
-  .setValue('#search_form_input_homepage', 'WebdriverIO')
-  .click('#search_button_homepage')
+  .url('https://frontendmasters.com/login/')
+  .setValue('#username', 'nhquocvinh')
+  .setValue('#password', '08041990')
+  .click('button[type=submit]')
   .getTitle().then(function (title) {
     console.log('Title is: ' + title);
   })
-  .end();
+  .url('https://frontendmasters.com/courses/javascript-hard-parts/')
+  .getTitle().then(function (title) {
+    console.log('Title is: ' + title);
+  })
+  .elements('section').then(function (res) {
+    console.log(res);
+
+    browser.elements(res[1]).then(function (sec) {
+      console.log(sec);
+    });
+  })
+  ;
